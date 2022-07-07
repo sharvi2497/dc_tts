66018a5f325e73dcb2b81e0a4d77d1b450456215
sharvi2497 <105599335+sharvi2497@users.noreply.github.com>
2022-07-07 11:41:16 -0500

    Update cleaners.py

--- text/cleaners.py
+++ text/cleaners.py
@@ -14,7 +14,7 @@ hyperparameter. Some cleaners are English-specific. You'll typically want to use
 
 import re
 from unidecode import unidecode
-from .numbers import normalize_numbers
+from dc_tts.text.numbers import normalize_numbers
 
 
 # Regular expression matching whitespace:
