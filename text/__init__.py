e30561d8a3db2f5e7657c6bce977a7de42cfa024
sharvi2497 <105599335+sharvi2497@users.noreply.github.com>
2022-07-07 11:40:41 -0500

    Update __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -1,7 +1,7 @@
 """ from https://github.com/keithito/tacotron """
 import re
-from text import cleaners
-from text.symbols import symbols
+from dc_tts.text import cleaners
+from dc_tts.text.symbols import symbols
 
 
 # Mappings from symbol to numeric ID and vice versa:
