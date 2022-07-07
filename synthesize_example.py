f538704728a2ede81df259ed5f35fe5e59dcd496
sharvi2497 <105599335+sharvi2497@users.noreply.github.com>
2022-07-07 10:55:21 -0500

    Update synthesize_example.py

--- synthesize_example.py
+++ synthesize_example.py
@@ -1,31 +1,46 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-import os
+"""
+This code file runs an example of synthesize
+params: 
+"""
+
 import sounddevice
 import soundfile
-import time
-from hyperparams import Hyperparams as hp
 from synthesize import Synthesizer
+import time
 
-# Update the following two paths:
-CHECKPOINT_TEXT2MEL = "models/LJ01-1/model_gs_1450k"
-CHECKPOINT_SSRN = "models/LJ01-2/model_gs_767k"
 
-def playsound(filename):
-	data, fs = soundfile.read(filename, dtype='float32')  
-	sounddevice.play(data, fs)
-	status = sounddevice.wait()
+# default values
+CHECKPOINT_TEXT2MEL = "models/LJ01-1/model_gs_2280k"
+CHECKPOINT_SSRN = "models/LJ01-2/model_gs_132k"
 
-if __name__ == "__main__":
-	synthesizer = Synthesizer(CHECKPOINT_TEXT2MEL, CHECKPOINT_SSRN)
-	filename = "utterance.wav"
-	while True:
-		utterance = raw_input("Enter an utterance: ")
-		if not utterance:
-			break
-		current_time = time.strftime("%Y:%m:%d-%H:%M:%S")
-		filename = f'utterance-{current_time}.wav'	
-		synthesizer.synthesize(utterance, filename)
-		print("Utterance saved in %s" % filename)
-		playsound(filename)
+
+def playsound(filename: str):
+    """This function plays the sound of the given filename
+    params : filename : string of file location
+    """
+    data, fs = soundfile.read(filename, dtype='float32')
+    sounddevice.play(data, fs)
+    status = sounddevice.wait()
+
+def run(checkpoint_text2mel, checkpoint_ssrn):
+    """this function
+    params: checkpoint_text2mel: string - location of Text2Mel model
+    checkpoint_ssrn: string - location of SSRN model
+    """
+    if checkpoint_text2mel and checkpoint_ssrn:
+        synthesizer = Synthesizer(checkpoint_text2mel, checkpoint_ssrn)
+    else:
+        synthesizer = Synthesizer(CHECKPOINT_TEXT2MEL, CHECKPOINT_SSRN)
+    while True:
+        utterance = input("Enter an utterance: ")
+        if not utterance:
+            break
+        current_time = time.strftime("%Y:%m:%d-%H:%M:%S")
+        filename = f'utterance-{current_time}.wav'
+        synthesizer.synthesize(utterance, filename)
+        print("Utterance saved in %s" % filename)
+        playsound(filename)
+
